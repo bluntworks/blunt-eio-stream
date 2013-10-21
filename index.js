f389b25860b2a1b7e5fdf3c7ab15d2baa74546a9
Dave Ryan <dave@bluntworks.net>
2013-10-21 11:39:17 +0100

    working on server

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var log     = require('bitlog')
 var thru    = require('through')
 var eiojson = require('eiojson')
 
@@ -6,8 +7,11 @@ var o = module.exports = function(socket) {
   var s = thru(function(data) { this.queue(data) })
   eiojson(socket)
   socket.on('json', function(data) {
-    if(!data._sok) data._sok = socket
-    s.write(data)
+    var o = {
+      _sok: socket,
+      body: data
+    }
+    s.write(o)
   })
   return s
 }
@@ -17,6 +21,7 @@ var o = module.exports = function(socket) {
 //Get Session info
 //TODO move this to sep module
 var cookie = require('cookie')
+var sig    = require('cookie-signature')
 
 function getsid(cookies, cfg) {
   var sid = cookies['connect.sid']
@@ -25,23 +30,24 @@ function getsid(cookies, cfg) {
     : sid
 }
 
-
 o.session = function(app) {
-  var s = thru(function(o) {
-    var _sok    = o._sok
+  var s = thru(function(req) {
+    var _sok    = req._sok
     var cookies = cookie.parse(_sok.request.headers.cookie)
     var sid     = getsid(cookies, app.cfg)
     var sdb     = app.sdb.sublevel('sessions')
     var users   = app.users
 
+    var self = this
     sdb.get(sid, function(err, sess) {
       if(err || !sess) return self.emit('error', new Error('no session'))
       users.get(sess.user, function(err, u) {
         if(err || !u) return self.emit('error', new Error('no user'))
         delete u.salt
-        o._user = u
+        req._user = u
+        req.app = app
         //pass it on
-        self.queue(o)
+        self.queue(req)
       })
     })
   })
@@ -49,5 +55,35 @@ o.session = function(app) {
   return s
 }
 
+o.router = function() {
+  var rooster = require('rooster')()
+
+  var res = thru(function(data) {
+    log('res', data)
+    s.queue(data)
+  })
+
+  var s = thru(function(req) {
+    var self = this
+    Object.keys(req.body).forEach(function(verb) {
+      //log('loop over verbs', verb)
+      var ctx = rooster.test(verb, req.body[verb]);
+      if(ctx) ctx.fn.call(ctx, req, res)
+      else log.err('unknown route', req._sok.id, req.body, req._user)
+    })
+  })
+
+  s.api = rooster
+  return s
+}
+
+o.out = function(socket) {
+  //eiojson(socket)
+  return thru(function(res) {
+    log('out', res)
+    socket.json(res)
+  })
+
+}
 
 
